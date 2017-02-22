e1d4c9aff28614039e1b40589abce887ad501648
qili <qili@pptv.com>
2017-02-22 18:01:50 +0800

    clean useless line

--- md5_route.sh
+++ md5_route.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#/usr/bin/md5sum `ls /home/route/NewRoute/* | grep -v md5`> /home/route/NewRoute/md5_route
 
 for TXT in $(ls /home/route/NewRoute/*.txt | grep -v md5); do
  md5sum ${TXT} | awk '{print $1}' > ${TXT}.md5
