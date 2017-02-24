e78ac477946f994786725b846b6afb889282c8ff
qili <qili@shnj-b-cobbler-201-144.osp-cob-ble.lin.idc.pplive.cn>
2017-02-24 17:53:48 +0800

    Add Rsync Old Program and New Modify_Crontab

--- NewRoute/modify_route.sh
+++ NewRoute/modify_route.sh
@@ -2,7 +2,9 @@
 source /home/route/NewRoute/Route_function.shell
 
 Download_Netfile && Updata_Program && Diff_Netfile
+Modify_Crontab
+Rsync_Old_Program
 Judge_Bond
 Choose_JF
-echo $Gwct $Gwcu $Gwcm $Gwtt
+echo $Gwct $Gwcu $Gwcm $Gwtt $JiFang
 Modify_ROUTE
